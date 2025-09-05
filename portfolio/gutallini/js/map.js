84d395abc0cd10a9dae6950509b43f6049b366bb
Sergei Ermilov <sergeiuzabila@gmail.com>
2025-09-06 01:42:58 +0300

    removed api key

--- portfolio/gutallini/js/map.js
+++ portfolio/gutallini/js/map.js
@@ -296,31 +296,3 @@ function google_maps_init() {
   markerElement.style.opacity = 1
 
 }
-
-function google_maps_lazyload(api_key) {
-  'use strict'
-
-  if (api_key) {
-    var options = {
-      rootMargin: '100px',
-      threshold: 0
-    }
-
-    var observer = new IntersectionObserver(
-      function(entries, self) {
-        var isIntersecting = typeof entries[0].isIntersecting === 'boolean' ? entries[0].isIntersecting : entries[0].intersectionRatio > 0
-        if (isIntersecting) {
-          var mapsJS = document.createElement('script')
-          mapsJS.src = 'https://maps.googleapis.com/maps/api/js?callback=google_maps_init&key=' + api_key
-          document.getElementsByTagName('head')[0].appendChild(mapsJS)
-          self.unobserve(map)
-        }
-      },
-      options
-    )
-
-    observer.observe(map)
-  }
-}
-
-google_maps_lazyload("AIzaSyB7ft0N1VoWIsUwzWbLUYqiXUj_uZzjJVA");
